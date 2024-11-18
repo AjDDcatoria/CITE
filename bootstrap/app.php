9978df687f0273f2e8ffc896e42840c9bde3ae09
AJ DDcatoria <ajdedicatoria9@gmail.com>
2024-11-18 12:58:25 +0800

    fix: exclude Livewire routes from CSRF validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => EnsureAdmin::class
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+           'livewire/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf_token" value="{{ csrf_token() }}"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="icon" href="{{ asset('image/NEMSU.png')  }}" type="image/png">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -17,6 +17,7 @@
 
     {{ $slot }}
 
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewireScripts
 </body>
 </html>
